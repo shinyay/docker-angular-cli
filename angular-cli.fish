bb7eb4c371996079c889bf5e97ce913ea6c41115
shinyay <shinya.com@gmail.com>
2019-04-22 20:21:24 +0900

    Add a script for angular cli

---
+++ angular-cli.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+
+function run_docker
+  argparse -n run_docker 'h/help' -- $argv
+  or return 1
+
+  if set -lq _flag_help
+    echo "angular-cli.fish <COMMAND>"
+    return
+  end
+  docker run --rm -it -v (pwd)/angularclient:/app shinyay/angular-cli $argv
+end
+
+run_docker $argv
