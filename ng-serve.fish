3f1982dda26d0ecc03f784e9569ef238545e5e26
shinyay <shinya.com@gmail.com>
2019-04-22 21:42:52 +0900

    Add starting angular test server

---
+++ ng-serve.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+if count $argv > /dev/null
+  docker run --rm -it -v (pwd)/$argv[1]:/app -p 4200:4200 shinyay/angular-cli ng serve --host 0.0.0.0 --open
+else
+  echo "ng-serve.fish <APP_DIR>"
+end
+
