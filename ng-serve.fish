64c5113003bee6bc87a553120d80afa86584c7be
shinyay <shinya.com@gmail.com>
2019-04-22 21:47:43 +0900

    Modify polling time for watchin application change

--- ng-serve.fish
+++ ng-serve.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 if count $argv > /dev/null
-  docker run --rm -it -v (pwd)/$argv[1]:/app -p 4200:4200 shinyay/angular-cli ng serve --host 0.0.0.0 --open
+  docker run --rm -it -v (pwd)/$argv[1]:/app -p 4200:4200 shinyay/angular-cli ng serve --host 0.0.0.0 --poll=2000
 else
   echo "ng-serve.fish <APP_DIR>"
 end
